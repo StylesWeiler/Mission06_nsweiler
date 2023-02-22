45df198866996742d1015fe6690db89f14e2f84e
Styles Weiler <19nstylesweiler@gmail.com>
2023-02-21 19:10:23 -0700

    Added new error message

--- Mission6_nsweiler/Models/MovieResponse.cs
+++ Mission6_nsweiler/Models/MovieResponse.cs
@@ -32,8 +32,8 @@ namespace Mission6_nsweiler.Models
         public bool edited { get; set; }
 
         // Build the foreign key relationship
-        [Required]
-        public int categoryId { get; set; } // foreign key
+        [Required (ErrorMessage = "The category field is required.")]
+        public int? categoryId { get; set; } // foreign key
         public Category category { get; set; } // paired with an instance of the Category type creates a connection between the two tables
     }
 }
